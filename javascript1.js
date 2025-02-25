78e221434a88121d67f5946e20275ac561de4fda
Jurre2002 <71696161+Jurre2002@users.noreply.github.com>
2025-02-25 16:29:11 +0100

    Jurrasic park foto toegevoegd + links naar de activiteiten op de map.

--- javascript1.js
+++ javascript1.js
@@ -1,87 +1,88 @@
-document.getElementById("knop1").addEventListener("click", function() {
-    document.querySelector(".container2").style.display = "block";
-    document.querySelector(".container3").style.display = "none";
-    document.querySelector(".container4").style.display = "none";
-    document.querySelector(".container5").style.display = "none";
-    document.querySelector(".container6").style.display = "none";
-});
+// document.getElementById("knop1").addEventListener("click", function() {
+//     document.querySelector(".container2").style.display = "block";
+//     document.querySelector(".container3").style.display = "none";
+//     document.querySelector(".container4").style.display = "none";
+//     document.querySelector(".container5").style.display = "none";
+//     document.querySelector(".container6").style.display = "none";
+// });
 
 
-document.getElementById("knop2").addEventListener("click", function() {
-    document.querySelector(".container2").style.display = "none";
-    document.querySelector(".container3").style.display = "block";
-    document.querySelector(".container4").style.display = "none";
-    document.querySelector(".container5").style.display = "none";
-    document.querySelector(".container6").style.display = "none";
-});
+// document.getElementById("knop2").addEventListener("click", function() {
+//     document.querySelector(".container2").style.display = "none";
+//     document.querySelector(".container3").style.display = "block";
+//     document.querySelector(".container4").style.display = "none";
+//     document.querySelector(".container5").style.display = "none";
+//     document.querySelector(".container6").style.display = "none";
+// });
 
-document.getElementById("knop3").addEventListener("click", function() {
-    document.querySelector(".container2").style.display = "none";
-    document.querySelector(".container3").style.display = "none";    
-    document.querySelector(".container4").style.display = "block";
-    document.querySelector(".container5").style.display = "none";
-    document.querySelector(".container6").style.display = "none";
-});
+// document.getElementById("knop3").addEventListener("click", function() {
+//     document.querySelector(".container2").style.display = "none";
+//     document.querySelector(".container3").style.display = "none";    
+//     document.querySelector(".container4").style.display = "block";
+//     document.querySelector(".container5").style.display = "none";
+//     document.querySelector(".container6").style.display = "none";
+// });
 
-document.getElementById("knop4").addEventListener("click", function() {
-    document.querySelector(".container2").style.display = "none";
-    document.querySelector(".container3").style.display = "none";    
-    document.querySelector(".container4").style.display = "none";
-    document.querySelector(".container5").style.display = "block";
-    document.querySelector(".container6").style.display = "none";
-});
+// document.getElementById("knop4").addEventListener("click", function() {
+//     document.querySelector(".container2").style.display = "none";
+//     document.querySelector(".container3").style.display = "none";    
+//     document.querySelector(".container4").style.display = "none";
+//     document.querySelector(".container5").style.display = "block";
+//     document.querySelector(".container6").style.display = "none";
+// });
 
-document.getElementById("knop5").addEventListener("click", function() {
-    document.querySelector(".container2").style.display = "none";
-    document.querySelector(".container3").style.display = "none";
-    document.querySelector(".container4").style.display = "none";
-    document.querySelector(".container5").style.display = "none";
-    document.querySelector(".container6").style.display = "block";
-});
-
-
-const closeButtons = document.querySelectorAll(".close-Button");
+// document.getElementById("knop5").addEventListener("click", function() {
+//     document.querySelector(".container2").style.display = "none";
+//     document.querySelector(".container3").style.display = "none";
+//     document.querySelector(".container4").style.display = "none";
+//     document.querySelector(".container5").style.display = "none";
+//     document.querySelector(".container6").style.display = "block";
+// });
 
 
-closeButtons.forEach(function(button) {
-    button.addEventListener("click", function(event) {
-        event.preventDefault(); 
+// const closeButtons = document.querySelectorAll(".close-Button");
+
+
+// closeButtons.forEach(function(button) {
+//     button.addEventListener("click", function(event) {
+//         event.preventDefault(); 
        
-        button.closest("div").style.display = "none"; 
-    });
-});
+//         button.closest("div").style.display = "none"; 
+//     });
+// });
 
-// Functie om de inhoud van een container op te slaan in sessionStorage
-function saveContent(containerClass, key) {
-    const content = document.querySelector(`.${containerClass} h1`).textContent; 
-    sessionStorage.setItem(key, content);
-}
+// // Functie om de inhoud van een container op te slaan in sessionStorage
+// function saveContent(containerClass, key) {
+//     const content = document.querySelector(`.${containerClass} h1`).textContent; 
+//     sessionStorage.setItem(key, content);
+// }
 
-// Event listeners voor de knoppen
-document.getElementById("knop1").addEventListener("click", function(event) {
-    event.preventDefault();
-    saveContent("container2", "content1");
-});
+// // Event listeners voor de knoppen
+// document.getElementById("knop1").addEventListener("click", function(event) {
+//     event.preventDefault();
+//     saveContent("container2", "content1");
+// });
 
-document.getElementById("knop2").addEventListener("click", function(event) {
-    event.preventDefault();
-    saveContent("container3", "content2");
-});
+// document.getElementById("knop2").addEventListener("click", function(event) {
+//     event.preventDefault();
+//     saveContent("container3", "content2");
+// });
 
-document.getElementById("knop3").addEventListener("click", function(event) {
-    event.preventDefault();
-    saveContent("container4", "content3");
-});
+// document.getElementById("knop3").addEventListener("click", function(event) {
+//     event.preventDefault();
+//     saveContent("container4", "content3");
+// });
 
-document.getElementById("knop4").addEventListener("click", function(event) {
-    event.preventDefault();
-    saveContent("container5", "content4");
-});
+// document.getElementById("knop4").addEventListener("click", function(event) {
+//     event.preventDefault();
+//     saveContent("container5", "content4");
+// });
 
-document.getElementById("knop5").addEventListener("click", function(event) {
-    event.preventDefault();
-    saveContent("container6", "content5");
-});
+// document.getElementById("knop5").addEventListener("click", function(event) {
+//     event.preventDefault();
+//     saveContent("container6", "content5");
+// });
+
 
 
 
